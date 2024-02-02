2b4eb5e6d95b7ce31196968d03a38c0a5e86e706
Jacob Smith <34405172+xcob@users.noreply.github.com>
2024-02-01 22:13:02 -0600

    no dots!

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -1,15 +1,7 @@
 <template>
   <main class="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
     <div class="p-24 md:order-1 hidden md:block">
-      <img
-        class="rounded-full transition-transform transform hover:scale-115"
-        src="~/assets/img/smith-dots.png"
-        alt="Starship starts the engine"
-        loading="eager"
-        format="avif"
-        width="512"
-        height="512"
-      />
+     
     </div>
 
     <div>
