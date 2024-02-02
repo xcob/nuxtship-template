2f8cc11995c3a365490166eba3978d035e186e27
Jacob Smith <34405172+xcob@users.noreply.github.com>
2024-02-02 17:05:14 -0600

    input name update

--- components/landing/Contactform.vue
+++ components/landing/Contactform.vue
@@ -103,20 +103,20 @@ onMounted(() => {
       <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border  focus:ring-4 ring-gray-100  rounded-lg sm:flex ">
         <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
             <div class="flex items-center ps-3">
-                <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100  border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 ">
+                <input id="vue-checkbox-list" type="checkbox" value="" name="Logo & Brand Design" class="w-4 h-4 text-blue-600 bg-gray-100  border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 ">
                 <label for="vue-checkbox-list" class="w-full py-3 ms-2 text-sm font-medium">Logo & Brand Design</label>
             </div>
         </li>
         <li class="w-full border-b  sm:border-b-0 sm:border-r ">
             <div class="flex items-center ps-3">
-                <input id="react-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                <label for="react-checkbox-list" class="w-full py-3 ms-2 text-sm font-medium">Web Development</label>
+                <input id="react-checkbox-list-2" type="checkbox" value="" name="Web Development" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                <label for="react-checkbox-list-2" class="w-full py-3 ms-2 text-sm font-medium">Web Development</label>
             </div>
         </li>
         <li class="w-full border-b sm:border-b-0 sm:border-r ">
             <div class="flex items-center ps-3">
-                <input id="angular-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                <label for="angular-checkbox-list" class="w-full py-3 ms-2 text-sm font-medium ">Social Media Graphics</label>
+                <input id="angular-checkbox-list-3" type="checkbox" value="" name="Social Media Graphics" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                <label for="angular-checkbox-list-3" class="w-full py-3 ms-2 text-sm font-medium ">Social Media Graphics</label>
             </div>
         </li>
       </ul>
